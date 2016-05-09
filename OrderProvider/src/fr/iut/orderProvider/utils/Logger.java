f499516f10d60e67b275d95ecc29aea061d14459
Michel <fiegelmichel@orange.fr>
2016-05-09 18:29:49 +0200

    Ajout du Logger et suppression du dossier test

---
+++ OrderProvider/src/fr/iut/orderProvider/utils/Logger.java
@@ -0,0 +1,46 @@
+package fr.iut.orderProvider.utils;
+
+public class Logger {
+
+	private static boolean printDebug = false;
+
+	/**
+	 * A simple message logging function. The message type gets printed before
+	 * the actual message.
+	 * 
+	 * @param msgType
+	 *            the message type
+	 * @param msg
+	 *            the message to display
+	 */
+	static public void log(String msgType, String msg) {
+		// If we're ignoring debug messages and this one is a debug
+		// message, return.
+		if (!printDebug && msgType.equals("debug")) {
+			return;
+		}
+
+		String finalMessage = "[OrderProvider";
+
+		if (!msgType.equals("")) {
+			finalMessage = finalMessage + " " + msgType;
+		}
+
+		finalMessage = finalMessage + "] " + msg;
+		System.out.println(finalMessage);
+		System.out.flush();
+	}
+
+	/// Sets how much debug output to print ('true' prints debug and error
+	/// messages; 'false' prints only error messages).
+	/**
+	 * Sets how much debug output to print ('true' prints debug and error
+	 * messages; 'false' prints only error messages).
+	 * 
+	 * @param enablePrintDebug
+	 */
+	static public void setPrintDebug(boolean enablePrintDebug) {
+		printDebug = enablePrintDebug;
+	}
+
+}
